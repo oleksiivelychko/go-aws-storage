95db2cff79329831f1e509c42b436e40ac3f3d41
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-10-20 20:06:36 +0300

    initialize cobra

---
+++ cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Short: "A brief description of your application",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config.yaml", "config YAML file")
+}
+
+func initConfig() {
+	viper.SetConfigFile(cfgFile)
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		_, _ = fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/oleksiivelychko/go-aws-storage/cmd"
+
+func main() {
+	cmd.Execute()
+}
