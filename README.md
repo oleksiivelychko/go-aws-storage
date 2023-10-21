366b330e6276060d56439b7bb21fec894f416cb2
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-10-21 11:11:38 +0300

    create-bucket command

--- README.md
+++ README.md
@@ -2,16 +2,27 @@
 
 ### (local) usage of AWS S3 via Cobra and/or AWS CLI.
 
+- create bucket via AWS CLI
+```
+aws s3api create-bucket --bucket my-bucket --endpoint-url http://localhost:4566 --profile localstack
+```
+- create bucket via CLI
+```
+go run main.go --config=config.yaml create-bucket --name=my-bucket [--isPublic=true]
+```
+---
+
 - list buckets via AWS CLI
 ```
 aws s3api list-buckets --endpoint-url http://localhost:4566 --profile localstack
 ```
 - list buckets via CLI
 ```
-go run main.go list-buckets
+go run main.go --config=config.yaml list-buckets
 ```
 ---
 
 ---
 ※ References:
 - [s3api](https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3api/index.html)
+- [Bucket naming rules](https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html)
---
+++ cmd/createBucket.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/oleksiivelychko/go-aws-storage/service"
+	"github.com/spf13/cobra"
+)
+
+var createBucketCmd = &cobra.Command{
+	Use:   "create-bucket",
+	Short: "Creates a new S3 bucket.",
+	Run: func(cmd *cobra.Command, args []string) {
+		storage, err := service.New(cfgAWS)
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		isPublic, err := cmd.Flags().GetBool("isPublic")
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		output, err := storage.CreateBucket(cmd.Flag("name").Value.String(), isPublic)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("%s\n%s\n", output, SuccessfulMessage)
+		}
+	},
+}
+
+func init() {
+	createBucketCmd.Flags().String("name", "", "")
+	createBucketCmd.Flags().Bool("isPublic", false, "make bucket a public (read-only)")
+
+	_ = createBucketCmd.MarkFlagRequired("name")
+
+	rootCmd.AddCommand(createBucketCmd)
+}
